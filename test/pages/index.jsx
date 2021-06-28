03130b27fa37697c3e9b0c9f5db8ed222d570762
sujiaheng <648952316@qq.com>
2021-06-28 10:09:04 +0800

    chore: remove room uuid & roomToken

--- test/pages/index.jsx
+++ test/pages/index.jsx
@@ -2,7 +2,7 @@ import { WhiteWebSdk } from "white-web-sdk";
 import { SceneCameraManger } from "../../dist";
 
 const sdk = new WhiteWebSdk({
-    appIdentifier: "-OePAJTDEequWX3apwJAEg/m5-NmP5HtHNyVA",
+    appIdentifier: "appid",
 })
 
 setTimeout(() => {
@@ -11,24 +11,13 @@ setTimeout(() => {
 
 const joinRoom = () => {
     sdk.joinRoom({
-        uuid: "ac530cf0d58611eb8d5c639e3db535ac",
-        roomToken: "NETLESSROOM_YWs9VWtNUk92M1JIN2I2Z284dCZub25jZT0xNjI0NjA2MDA0MTc0MDAmcm9sZT0wJnNpZz0yZWVlMTNiNzBiN2EwZjdkNjUwZjZlZTJiNmI0ZTkzN2Q5YjQ3MmI1YjBmYzZmYmJmNjg4M2QxZWQwNWExM2ZiJnV1aWQ9YWM1MzBjZjBkNTg2MTFlYjhkNWM2MzllM2RiNTM1YWM"
+        uuid: "room uuid",
+        roomToken: "room Token"
     }).then(room => {
         room.bindHtmlElement(document.getElementById("whiteboard"));
         window.room = room;
         const manager = new SceneCameraManger(room, "test1");
         window.manager = manager;
-        // const plugin = room.getInvisiblePlugin(IframeBridge.kind)
-        // window.plugin = plugin;
-        // if (!plugin) {
-        //     IframeBridge.insert({ // 插件插入到白板中
-        //         room,
-        //         width: 1280,
-        //         height: 720,
-        //         url: "https://demo-h5.netless.group/dist2020/", // iframe url
-        //         displaySceneDir: "/example"
-        //     })
-        // }
     })
 }
 
